2ea7ffed57ec1d9041abd0b3de41e89f0617bd3a
freelon <freelon@github>
2020-12-01 08:07:53 +0100

    cleanup.

--- src/main/kotlin/com/github/freelon/aoc2020/day01.kt
+++ src/main/kotlin/com/github/freelon/aoc2020/day01.kt
@@ -8,13 +8,13 @@ fun main() {
     secondHalf(numbers)
 }
 
-internal fun firstHalf(numbers: List<Int>) {
+private fun firstHalf(numbers: List<Int>) {
     val a = numbers.first { a -> numbers.any { b -> a + b == 2020 } }
     val b = numbers.first { b -> a + b == 2020 }
     println("a * b = ${a * b}")
 }
 
-internal fun secondHalf(numbers: List<Int>) {
+private fun secondHalf(numbers: List<Int>) {
     val a = numbers.first { a -> numbers.any { b -> numbers.any { c -> a + b + c == 2020 } } }
     val b = numbers.first { b -> numbers.any { c -> a + b + c == 2020 } }
     val c = numbers.first { c -> a + b + c == 2020 }
---
+++ src/main/kotlin/com/github/freelon/aoc2020/day02.kt
@@ -0,0 +1,15 @@
+package com.github.freelon.aoc2020
+
+import java.io.File
+
+fun main() {
+    val input = File("input/day02.txt").readLines()
+    firstHalf()
+    secondHalf()
+}
+
+private fun firstHalf() {
+}
+
+private fun secondHalf() {
+}
