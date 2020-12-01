492ef79bf16f04994cadfe5b432d61bc72c4dee2
freelon <freelon@github>
2020-12-01 08:06:13 +0100

    Initial commit - day 1.

---
+++ src/main/kotlin/com/github/freelon/aoc2020/day01.kt
@@ -0,0 +1,22 @@
+package com.github.freelon.aoc2020
+
+import java.io.File
+
+fun main() {
+    val numbers = File("input/day01.txt").readLines().map { Integer.parseInt(it) }
+    firstHalf(numbers)
+    secondHalf(numbers)
+}
+
+internal fun firstHalf(numbers: List<Int>) {
+    val a = numbers.first { a -> numbers.any { b -> a + b == 2020 } }
+    val b = numbers.first { b -> a + b == 2020 }
+    println("a * b = ${a * b}")
+}
+
+internal fun secondHalf(numbers: List<Int>) {
+    val a = numbers.first { a -> numbers.any { b -> numbers.any { c -> a + b + c == 2020 } } }
+    val b = numbers.first { b -> numbers.any { c -> a + b + c == 2020 } }
+    val c = numbers.first { c -> a + b + c == 2020 }
+    println("a * b * c = ${a * b * c}")
+}
