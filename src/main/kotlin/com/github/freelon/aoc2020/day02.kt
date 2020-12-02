22bd662a48e4d5ea49e8dd0e689578cb4f0e8053
freelon <freelon@github>
2020-12-02 09:45:35 +0100

    Day 2.

--- src/main/kotlin/com/github/freelon/aoc2020/day02.kt
+++ src/main/kotlin/com/github/freelon/aoc2020/day02.kt
@@ -4,12 +4,43 @@ import java.io.File
 
 fun main() {
     val input = File("input/day02.txt").readLines()
-    firstHalf()
-    secondHalf()
+
+    val regex = Regex("(\\d+)-(\\d+) (\\w): (\\w+)")
+    val passwords = input.filter { regex.matches(it) }
+        .map {
+            val matchEntire = regex.matchEntire(it)!!
+            Line(
+                Integer.parseInt(matchEntire.groupValues[1]),
+                Integer.parseInt(matchEntire.groupValues[2]),
+                matchEntire.groupValues[3][0],
+                matchEntire.groupValues[4]
+            )
+        }
+
+    firstHalf(passwords)
+    secondHalf(passwords)
 }
 
-private fun firstHalf() {
+private fun firstHalf(input: List<Line>) {
+    val count = input
+        .filter(Line::isPasswordOkayByOldPolicy)
+        .count()
+
+    println("Count part 1: $count")
 }
 
-private fun secondHalf() {
+private fun secondHalf(input: List<Line>) {
+    val count = input
+        .filter(Line::isPasswordOkayTobogganPolicy)
+        .count()
+
+    println("Count part 1: $count")
+}
+
+data class Line(val nMin: Int, val nMax: Int, val c: Char, val password: String) {
+
+    fun isPasswordOkayByOldPolicy(): Boolean =
+        password.count { it == c } in nMin..nMax
+
+    fun isPasswordOkayTobogganPolicy(): Boolean = (password[nMin - 1] == c) xor (password[nMax - 1] == c)
 }
