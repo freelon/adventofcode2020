ae9319d57ceb2bb727dc7130ad8c260976678061
freelon <freelon@github>
2020-12-06 08:51:59 +0100

    build: Added test stuff.

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,17 @@ version = "1.0-SNAPSHOT"
 repositories {
     mavenCentral()
 }
+
 dependencies {
     testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
+
+tasks.test {
+    useJUnitPlatform()
+}
+
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
