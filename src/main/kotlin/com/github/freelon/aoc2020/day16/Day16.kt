deaa161af2024a111b9b6aa90bbfc461bb0ab35e
Johannes <freelon@gmx.net>
2023-07-03 13:04:44 +0200

    Day 16 part 2

--- src/main/kotlin/com/github/freelon/aoc2020/day16/Day16.kt
+++ src/main/kotlin/com/github/freelon/aoc2020/day16/Day16.kt
@@ -2,8 +2,18 @@ package com.github.freelon.aoc2020.day16
 
 import com.github.freelon.aoc2020.DayTemplate
 
+
+fun main() {
+    Day16().run()
+}
+
+data class Field(val name: String, val restriction1: LongRange, val restriction2: LongRange)
+
+data class Ticket(val values: List<Long>)
+
 class Day16 : DayTemplate() {
-    override fun partOne(input: String): Int {
+
+    override fun partOne(input: String): Long {
         val (fields, _, nearbyTickets) = parse(input)
         val invalidValues = nearbyTickets.flatMap { ticket ->
             val badTicketValues =
@@ -13,8 +23,21 @@ class Day16 : DayTemplate() {
         return invalidValues
     }
 
-    override fun partTwo(input: String): Any {
-        TODO("Not yet implemented")
+    override fun partTwo(input: String): Long {
+        val (fields, myTicket, nearbyTickets) = parse(input)
+        val tickets = listOf(myTicket) + nearbyTickets
+        val validTickets =
+            tickets.filter { ticket ->
+                ticket.values.all { v ->
+                    fields.any { field -> (v in field.restriction1) or (v in field.restriction2) }
+                }
+            }
+
+        val assignments = fieldAssignments(fields, validTickets)
+        return myTicket.values.zip(assignments)
+            .filter { it.second.name.startsWith("departure") }
+            .map { it.first }
+            .fold(1) { a, b -> a * b }
     }
 }
 
@@ -23,24 +46,41 @@ fun parse(input: String): Triple<List<Field>, Ticket, List<Ticket>> {
     val fields = a.lines().map { line ->
         val (name, rest) = line.split(":")
         val parts = rest.trim().split(" ")
-        val r1 = parts[0].split("-").map { it.toInt() }
-        val r2 = parts[2].split("-").map { it.toInt() }
+        val r1 = parts[0].split("-").map { it.toLong() }
+        val r2 = parts[2].split("-").map { it.toLong() }
         Field(name, r1[0]..r1[1], r2[0]..r2[1])
     }
 
-    val myTicket = Ticket(b.lines()[1].split(",").map { it.toInt() })
+    val myTicket = Ticket(b.lines()[1].split(",").map { it.toLong() })
 
     val nearbyTickets = c.trim().lines().drop(1).map { line ->
-        Ticket(line.split(",").map { it.toInt() })
+        Ticket(line.split(",").map { it.toLong() })
     }
 
     return Triple(fields, myTicket, nearbyTickets)
 }
 
-data class Field(val name: String, val restriction1: IntRange, val restriction2: IntRange)
-
-data class Ticket(val values: List<Int>)
-
-fun main() {
-    Day16().run()
+fun fieldAssignments(fields: List<Field>, tickets: List<Ticket>): List<Field> {
+    val possibleAssignments = fields.map { Pair(it, fields.indices.toMutableList()) }
+    for ((field, assignments) in possibleAssignments) {
+        assignments.removeIf { a ->
+            tickets.any { t ->
+                val valueAtPosition = t.values[a]
+                val restriction1Satisfied = valueAtPosition in field.restriction1
+                val restriction2satisfied = valueAtPosition in field.restriction2
+                !(restriction1Satisfied or restriction2satisfied)
+            }
+        }
+    }
+    while (possibleAssignments.any { it.second.size > 1 }) {
+        possibleAssignments
+            .filter { it.second.size == 1 }
+            .forEach { assignment ->
+                val (field, assignments) = assignment
+                val finalAssignment = assignments.first()
+                possibleAssignments.filter { it.first != field }
+                    .forEach { it.second.removeIf { vToCheck -> vToCheck == finalAssignment } }
+            }
+    }
+    return possibleAssignments.sortedBy { it.second.first() }.map { it.first }
 }
--- src/test/kotlin/com/github/freelon/aoc2020/day16/Day16Test.kt
+++ src/test/kotlin/com/github/freelon/aoc2020/day16/Day16Test.kt
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 class Day16Test {
-
     @Test
     fun part1() {
         val input = """class: 1-3 or 5-7
@@ -20,5 +19,24 @@ nearby tickets:
 55,2,20
 38,6,12"""
         assertEquals(71, Day16().partOne(input))
+    }
+
+    @Test
+    fun part2() {
+        val input = """class: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9"""
+        val (fields, myTicket, nearbyTickets) = parse(input)
+        val tickets = listOf(myTicket) + nearbyTickets
+
+        assertEquals(listOf(fields[1], fields[0], fields[2]), fieldAssignments(fields, tickets))
     }
 }
