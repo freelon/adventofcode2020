a70cda3e211a6e73502e412fb25e943175955b04
Johannes <freelon@gmx.net>
2023-07-03 11:08:08 +0200

    Day 13 part 2

--- src/main/kotlin/com/github/freelon/aoc2020/day13/day.kt
+++ src/main/kotlin/com/github/freelon/aoc2020/day13/day.kt
@@ -1,6 +1,8 @@
 package com.github.freelon.aoc2020.day13
 
 import com.github.freelon.aoc2020.DayTemplate
+import java.time.Duration
+import java.time.Instant
 
 class Day13 : DayTemplate() {
 
@@ -17,6 +19,35 @@ class Day13 : DayTemplate() {
     }
 
     override fun partTwo(input: String): Any {
-        return ""
+        Thread {
+            val start = Instant.now()
+            while (true) {
+                println("${Duration.between(start, Instant.now()).seconds}s -- %,d".format(foo))
+                Thread.sleep(1000)
+            }
+        }.start()
+        return solve(input.lines()[1].split(",").mapNotNull { it.toLongOrNull() }, offset = 175710168444071L)
     }
+}
+
+var foo = 0L
+
+fun solve(busses: List<Long?>, offset: Long = 100000000000000): Long {
+    val indexedRelevantBusses = busses
+        .withIndex()
+        .mapNotNull { if (it.value != null) IndexedValue(it.index, it.value!!) else null }
+    val mainBus = indexedRelevantBusses.maxBy { it.value }
+    val startTime = (offset + 1..Long.MAX_VALUE).first { time -> (time + mainBus.index.toLong()) % mainBus.value == 0L }
+    val times = LongRange(startTime, Long.MAX_VALUE).step(mainBus.value)
+    return times
+        .first { t ->
+            foo = t
+            indexedRelevantBusses.all { (index, bus) ->
+                (t + index) % bus == 0L
+            }
+        }
+}
+
+fun main() {
+    Day13().run()
 }
--- src/test/kotlin/com/github/freelon/aoc2020/Day13Test.kt
+++ src/test/kotlin/com/github/freelon/aoc2020/Day13Test.kt
@@ -1,6 +1,7 @@
 package com.github.freelon.aoc2020
 
 import com.github.freelon.aoc2020.day13.Day13
+import com.github.freelon.aoc2020.day13.solve
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -9,6 +10,24 @@ class Day13Test {
     fun part1() {
         val result = Day13().partOne(EXAMPLE_INPUT)
         assertEquals(295L, result)
+    }
+
+    @Test
+    fun `part1 value`() {
+        val result =
+            Day13().partOne("1000510\n19,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,523,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,17,13,x,x,x,x,x,x,x,x,x,x,29,x,853,x,x,x,x,x,37,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,23\n")
+        assertEquals(259L, result)
+    }
+
+    @Test
+    fun `part2 examples`() {
+        assertEquals(1068781L, solve(listOf(7, 13, null, null, 59, null, 31, 19), 0))
+
+        assertEquals(3417L, solve(listOf(17, null, 13, 19), 0))
+        assertEquals(754018L, solve(listOf(67, 7, 59, 61), 0))
+        assertEquals(779210L, solve(listOf(67, null, 7, 59, 61), 0))
+        assertEquals(1261476L, solve(listOf(67, 7, null, 59, 61), 0))
+        assertEquals(1202161486L, solve(listOf(1789, 37, 47, 1889), 0))
     }
 }
 
